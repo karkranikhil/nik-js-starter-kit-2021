376d5b1e4bd027fe45cd043fb75b715ade8ba58b
Nikhil Karkra <karkra.nikhil@gmail.com>
2021-07-15 10:42:44 +1000

    development web server and sharing work with url

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import path from 'path'
+import open from 'open'
+
+//Fetching Directory path
+const __dirname = path.resolve();
+
+//port on which server runs
+const PORT = 3000
+
+//initializing our express app
+const app = express()
+
+//Serving our src app
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, './src/index.html'))
+})
+
+//listening app and opening app
+app.listen(PORT, (err)=>{
+  if(err){
+    console.log(err)
+  } else {
+    open(`http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
+  }
+})
+
